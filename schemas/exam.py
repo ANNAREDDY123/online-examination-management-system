9585dde02db040ad3d0d2afd4f188fc3093ce3c6
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-17 10:13:45 +0530

    Create exam.py

---
+++ schemas/exam.py
@@ -0,0 +1,27 @@
+from pydantic import (
+    BaseModel,
+    Field
+)
+
+
+class ExamCreate(BaseModel):
+
+    title: str
+
+    category: str
+
+    duration: int = Field(
+        gt=0
+    )
+
+    is_active: bool = True
+
+
+class ExamResponse(
+    ExamCreate
+):
+
+    id: int
+
+    class Config:
+        from_attributes = True
